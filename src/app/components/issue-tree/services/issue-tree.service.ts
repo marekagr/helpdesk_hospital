7b93e35c5f7a9067684cfc2d99d81ce27ec2fc05
marekagr <marekagr@poczta.onet.pl>
2023-06-12 14:59:13 +0200

    hospital 12062023

--- src/app/components/issue-tree/services/issue-tree.service.ts
+++ src/app/components/issue-tree/services/issue-tree.service.ts
@@ -12,7 +12,8 @@ import {
   line,
   treemap,
   tree,
-  hierarchy
+  hierarchy,
+  selection
 } from "d3";
 
 @Injectable({
@@ -52,23 +53,59 @@ export class IssueTreeService {
 
 
 public update(source:any,svg: Selection<any, any, any, any>,treemap:any,root:any,rectWidth:number,rectHight:number,offsetYLink:number,duration:number,params:any) {
+  var nodeEnter:any
+  var node: Selection<any, any, any, any>;
+  let licz=0  
   var treeData = treemap(root);
 
 
-  var nodes = treeData.descendants(),
-  links = treeData.descendants().slice(1);
+    var nodes = treeData.descendants(),
+    links = treeData.descendants().slice(1);
 
+    this.setNodesYCoordinate(nodes)
 
-nodes.forEach((d:any)=> {
-  d.y = d.depth * 180;
-});
-let licz=0
-var node: Selection<any, any, any, any> = svg.selectAll("g.node").data(nodes, (d:any)=> {
-  let wynik=d.id || (d.id = ++params.licz);
-  licz++
-  return wynik
-});
-var nodeEnter = node
+    node=this.setNode(nodes,svg,params);
+
+    nodeEnter=this.nodeEnter(node,source,svg,treemap,root,rectWidth,rectHight,offsetYLink,duration,params)
+
+    //------------------ draw rectangle ---------------------------------
+    this.drawRect(nodeEnter,rectWidth,rectHight)
+
+    //-------------------- text -------------------------------------------------------------
+    this.drawText(nodeEnter)
+
+    //------------------------ add Icons & Menu ------------------------
+    this.addIconsAndMenu(nodeEnter,svg,treemap,root,rectWidth,rectHight,offsetYLink,duration,params)
+
+    //------------------------ node update -----------------------
+    this.updateNode(nodeEnter,node,source,duration)
+
+    //----------------------------start link ---------------------------
+    this.drawLinks(svg,links,source,rectWidth,offsetYLink,duration)
+
+    this.update0Coordinate(nodes)
+
+    console.log('nodes',nodes)
+
+}
+
+private setNodesYCoordinate(nodes:any){
+  nodes.forEach((d:any)=> {
+    d.y = d.depth * 180;
+  });
+}
+
+private setNode(nodes:any,svg: Selection<any, any, any, any>,params:any):Selection<any, any, any, any>{
+  return  svg.selectAll("g.node").data(nodes, (d:any)=> {
+    let wynik=d.id || (d.id = ++params.licz);
+    // licz++
+    return wynik
+  });
+
+}
+
+private nodeEnter(node: Selection<any, any, any, any>,source:any,svg: Selection<any, any, any, any>,treemap:any,root:any,rectWidth:number,rectHight:number,offsetYLink:number,duration:number,params:any):any{
+   let nodeEnter = node
   .enter()
   .append("g")
   .attr("class", "node")
@@ -81,7 +118,11 @@ var nodeEnter = node
 
   });
 
-//------------------ draw rectangle ---------------------------------
+  return nodeEnter
+}
+
+private drawRect(nodeEnter:any,rectWidth:number,rectHight:number){
+  //------------------ draw rectangle ---------------------------------
 // nodeEnter
 //   .attr("class", "node")
 //   .attr("r", 1e-6)
@@ -89,39 +130,41 @@ var nodeEnter = node
 //     return d.parent ? "rgb(39, 43, 77)" : "#fe6e9e";
 //   });
 nodeEnter
-  .append("rect")
-  .attr("rx", (d:any)=> {
-    if (d.parent) return d.children || d._children ? 0 : 6;
-    return 10;
-  })
-  .attr("ry", (d:any)=> {
-    if (d.parent) return d.children || d._children ? 0 : 6;
-    return 10;
-  })
-  .attr("stroke-width", (d:any)=> {
-    return d.parent ? 1 : 0;
-  })
-  .attr("stroke", (d:any)=> {
-    return d.children || d._children
-      ? "rgb(3, 192, 220)"
-      : "rgb(38, 222, 176)";
-  })
-  .attr("stroke-dasharray", (d:any)=> {
-    return d.children || d._children ? "0" : "2.2";
-  })
-  .attr("stroke-opacity", (d:any)=> {
-    return d.children || d._children ? "1" : "0.6";
-  })
-  .attr("x", 0)
-  .attr("y", -10)
-  .attr("width", (d:any)=> {
-    return d.parent ? rectWidth : rectWidth;
-  })
-  .attr("height", rectHight);
+.append("rect")
+.attr("rx", (d:any)=> {
+  if (d.parent) return d.children || d._children ? 0 : 6;
+  return 10;
+})
+.attr("ry", (d:any)=> {
+  if (d.parent) return d.children || d._children ? 0 : 6;
+  return 10;
+})
+.attr("stroke-width", (d:any)=> {
+  return d.parent ? 1 : 0;
+})
+.attr("stroke", (d:any)=> {
+  return d.children || d._children
+    ? "rgb(3, 192, 220)"
+    : "rgb(38, 222, 176)";
+})
+.attr("stroke-dasharray", (d:any)=> {
+  return d.children || d._children ? "0" : "2.2";
+})
+.attr("stroke-opacity", (d:any)=> {
+  return d.children || d._children ? "1" : "0.6";
+})
+.attr("x", 0)
+.attr("y", -10)
+.attr("width", (d:any)=> {
+  return d.parent ? rectWidth : rectWidth;
+})
+.attr("height", rectHight);
 //------------------------ end rectangle -------------------------------------
 
+}
 
-//-------------------- text -------------------------------------------------------------
+private drawText(nodeEnter:any){
+  //-------------------- text -------------------------------------------------------------
 nodeEnter
 .append("text")
 .style("fill", (d:any)=> {
@@ -134,40 +177,42 @@ return "rgb(39, 43, 77)";
 .attr("x", (d:any)=> {
 return d.children ? (d.data.value)*(1) : d.data.value+4
 })
-.attr("text-anchor", function(d) {
+.attr("text-anchor", (d:any)=> {
 return "middle";
 })
 .text((d:any)=> {
 return d.data.name;
 });
 //----------------- end text --------------------------------------------
+}
 
-
-//------------------------ start a href ------------------------
+private addIconsAndMenu(nodeEnter:any,svg: Selection<any, any, any, any>,treemap:any,root:any,rectWidth:number,rectHight:number,offsetYLink:number,duration:number,params:any){
+  //------------------------ start a href ------------------------
 nodeEnter
 .append("a")
 .attr("xlink:href", "http://en.wikipedia.org/wiki/")
 .on("click", (event:any)=>{
-event.preventDefault();
-event.stopPropagation();
-console.log("d3.event",event)
-let newNodeObj:any={ name: "A6",level: "blue",  value: 6,children: []}
-//Creates new Node
-var newNode:any = hierarchy(newNodeObj);
-newNode.depth = event.target.__data__.depth + 1;
-newNode.height = event.target.__data__.height - 1;
-newNode.parent = event.target.__data__;
-newNode.id = ++params.licz;
-event.target.__data__.children.push(newNode)
-event.target.__data__.data.children.push(newNode)
+  event.preventDefault();
+  event.stopPropagation();
+  console.log("d3.event",event)
+  let newNodeObj:any={ name: "A6",level: "blue",  value: 6,children: []}
+  //Creates new Node
+  var newNode:any = hierarchy(newNodeObj);
+  newNode.depth = event.target.__data__.depth + 1;
+  newNode.height = event.target.__data__.height - 1;
+  newNode.parent = event.target.__data__;
+  newNode.id = ++params.licz;
+  event.target.__data__.children.push(newNode)
+  event.target.__data__.data.children.push(newNode)
 
-//treeData.children[0].children?.push({ name: "A6",level: "blue",  value: 6})
-// this.root = hierarchy(this.treeData, function(d:any) {
-//   return d.children;
-// });
-this.update(event.target.__data__,svg,treemap,root,rectWidth,rectHight,offsetYLink,duration,params)
+  //treeData.children[0].children?.push({ name: "A6",level: "blue",  value: 6})
+  // this.root = hierarchy(this.treeData, function(d:any) {
+  //   return d.children;
+  // });
+  this.update(event.target.__data__,svg,treemap,root,rectWidth,rectHight,offsetYLink,duration,params)
 
-})
+  }
+)
 .append("image")
 .attr("xlink:href", "https://github.com/favicon.ico")
 .attr("x", 18)
@@ -175,76 +220,78 @@ this.update(event.target.__data__,svg,treemap,root,rectWidth,rectHight,offsetYLi
 .attr("width", 16)
 .attr("height", 16);
 //------------------- end a href -----------------------------------
+}
 
+private updateNode(nodeEnter:any,node: Selection<any, any, any, any>,source:any,duration:number){
+  //------------------------ node update -----------------------
+  var nodeUpdate = nodeEnter.merge(node);
 
-//------------------------ node update -----------------------
-var nodeUpdate = nodeEnter.merge(node);
-
-nodeUpdate
-.transition()
-.duration(duration)
-.attr("transform", (d:any)=> {
-  return "translate(" + d.y + "," + d.x + ")";
-});
-var nodeExit = node
-.exit()
-.transition()
-.duration(duration)
-.attr("transform", (d:any)=> {
-  return "translate(" + source.y + "," + source.x + ")";
-})
-.remove();
-nodeExit.select("rect").style("opacity", 1e-6);
-nodeExit.select("rect").attr("stroke-opacity", 1e-6);
-nodeExit.select("text").style("fill-opacity", 1e-6);
-
-
-//------------------------ end node update --------------------
-
-
-//----------------------------start link ---------------------------
-let link:Selection<any, any, any, any> = svg.selectAll("path.link").data(links, function(d:any) {
-return d.id;
-});
-var linkEnter = link
-.enter()
-.insert("path", "g")
-.attr("class", "link")
-.style("stroke", (d:any)=> { return d.data.level; })
-.attr("d", (d:any)=> {
- // var o = { x: source.x0, y: source.y0 };
-  // var o = { x: d.x+this.offsetYLink, y: d.y };
-  var o = { x: source.x0+offsetYLink, y: source.y0 };
-  // return this.diagonal(o, {x:d.parent.x+this.offsetYLink,y:d.parent.y+this.rectWidth});
-  return this.diagonal(o, {x:source.x0+offsetYLink,y:source.y0+rectWidth});
-
-});
-
-var linkUpdate = linkEnter.merge(link);
-linkUpdate
+  nodeUpdate
   .transition()
   .duration(duration)
-  .attr("d", (d:any)=> {
-    var o = { x: d.x+offsetYLink, y: d.y };
-    return this.diagonal(o, {x:d.parent.x+offsetYLink,y:d.parent.y+rectWidth});
+  .attr("transform", (d:any)=> {
+    return "translate(" + d.y + "," + d.x + ")";
   });
-var linkExit = link
+  var nodeExit = node
   .exit()
   .transition()
   .duration(duration)
-  .attr("d", (d:any)=> {
-  // var o = { x: source.x, y: source.y };
-    var o = { x: source.x+offsetYLink, y: source.y };
-    return this.diagonal(o, o);
+  .attr("transform", (d:any)=> {
+    return "translate(" + source.y + "," + source.x + ")";
   })
   .remove();
+  nodeExit.select("rect").style("opacity", 1e-6);
+  nodeExit.select("rect").attr("stroke-opacity", 1e-6);
+  nodeExit.select("text").style("fill-opacity", 1e-6);
 
+
+//------------------------ end node update --------------------
+}
+
+private drawLinks(svg: Selection<any, any, any, any>,links:any,source:any,rectWidth:number,offsetYLink:number,duration:number){
+  //----------------------------start link ---------------------------
+let link:Selection<any, any, any, any> = svg.selectAll("path.link").data(links, function(d:any) {
+  return d.id;
+  });
+  var linkEnter = link
+  .enter()
+  .insert("path", "g")
+  .attr("class", "link")
+  .style("stroke", (d:any)=> { return d.data.level; })
+  .attr("d", (d:any)=> {
+   // var o = { x: source.x0, y: source.y0 };
+    // var o = { x: d.x+this.offsetYLink, y: d.y };
+    var o = { x: source.x0+offsetYLink, y: source.y0 };
+    // return this.diagonal(o, {x:d.parent.x+this.offsetYLink,y:d.parent.y+this.rectWidth});
+    return this.diagonal(o, {x:source.x0+offsetYLink,y:source.y0+rectWidth});
+  
+  });
+  
+  var linkUpdate = linkEnter.merge(link);
+  linkUpdate
+    .transition()
+    .duration(duration)
+    .attr("d", (d:any)=> {
+      var o = { x: d.x+offsetYLink, y: d.y };
+      return this.diagonal(o, {x:d.parent.x+offsetYLink,y:d.parent.y+rectWidth});
+    });
+  var linkExit = link
+    .exit()
+    .transition()
+    .duration(duration)
+    .attr("d", (d:any)=> {
+    // var o = { x: source.x, y: source.y };
+      var o = { x: source.x+offsetYLink, y: source.y };
+      return this.diagonal(o, o);
+    })
+    .remove();
+}
+
+private update0Coordinate(nodes:any){
   nodes.forEach((d:any)=> {
     d.x0 = d.x;
     d.y0 = d.y;
   });
-  console.log('nodes',nodes)
-
 }
 
 private diagonal(s:any, p:any) {
@@ -269,7 +316,7 @@ private click(d:any,svg: Selection<any, any, any, any>,treemap:any,root:any,rect
 }
 
 
-  private collapse(d:any | null){
+private collapse(d:any | null){
     if (d.children) {
       d._children = d.children;
       d._children.forEach((n:any)=>{
